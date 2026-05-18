db82a695d9d6d3a7bb9a9583cebe49b6b1d2fd03
vlad595 <vladggaort@gmail.com>
2026-05-18 23:45:33 +0300

    Added grid to main screen

--- obj/Debug/net9.0/SpaceSimulator.AssemblyInfo.cs
+++ obj/Debug/net9.0/SpaceSimulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceSimulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+683e8acd29ac18ccc0fa58dc62b5614269584e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a0cde65d93e5e5f5658081fff28b72c58bdaba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceSimulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceSimulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- states/MainScreenState.cs
+++ states/MainScreenState.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Xml.Serialization;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -8,6 +9,7 @@ namespace SpaceSimulator.states
     public class MainScreenState : State
     {
         private Texture2D _pixel;
+        private int _gridStep;
         public MainScreenState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
         {
             
@@ -17,6 +19,8 @@ namespace SpaceSimulator.states
         {
             _pixel = new Texture2D(_graphicsDevice, 1, 1);
             _pixel.SetData(new[] { Color.White });
+
+            _gridStep = 25;
         }
 
         public override void Update(GameTime gameTime)
@@ -28,7 +32,27 @@ namespace SpaceSimulator.states
         {
             spriteBatch.Begin();
             
+            for (int i = 0; i < _graphicsDevice.Viewport.Width; i += _gridStep)
+            {
+                DrawLine(spriteBatch, new Vector2(i, 0), new Vector2(i, _graphicsDevice.Viewport.Height), Color.Gray);
+            }
+
+            for (int i = 0; i < _graphicsDevice.Viewport.Height; i += _gridStep)
+            {
+                DrawLine(spriteBatch, new Vector2(0, i), new Vector2(_graphicsDevice.Viewport.Width, i), Color.Gray);
+            }
+
             spriteBatch.End();
+        }
+
+        public void DrawLine(SpriteBatch spriteBatch, Vector2 start, Vector2 end, Color color, float thickness = 1.0f)
+        {
+
+            float distance = Vector2.Distance(start, end);
+
+            float angle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
+
+            spriteBatch.Draw(texture: _pixel, position: start, color: color, rotation: angle, origin: Vector2.Zero, scale: new Vector2(distance, thickness), layerDepth: 0f, effects: SpriteEffects.None, sourceRectangle: null);
         }
     }
 }
